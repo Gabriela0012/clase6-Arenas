32a311f76fcdb36638596ec03a1c2f9913bf413c
gabriela Arenas <gabega10@gmail.com>
2022-03-12 09:28:50 -0300

    localStorage

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+// Se le pide usuario que se registre
+// alert("Porfavor registrese para obtener un descuento")
+// let nombre = prompt("ingrese su nombre");
+//   console.log(nombre);
+// let apellido = prompt("ingrese apellido");
+//   console.log(apellido);
+// let nombreApellido = nombre + " " + apellido;
+//   console.log(nombreApellido);
+//   alert("¡Hola bienvenido" + " " + nombre + " " + apellido + "!" + "\nSu código es 'BIENVENIDO10'\n Porfavor haga click en productos");
+
+
+
+
+
+
+
